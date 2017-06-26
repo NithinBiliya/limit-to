5afb1e8fb874f8db1cad50622d335695fada596d
NithinBiliya <nithin.biliya@awpl.co>
2017-06-26 13:57:17 +0530

    fixed issue with late load of limit-to attribute value

--- dist/limitTo.js
+++ dist/limitTo.js
@@ -1,20 +1,20 @@
-(function() {
+(function () {
 
   angular.module('limit.to', [])
 
-  //Function to limit the Max Length entered
-  .directive("limitTo", [function() {
-    return {
-      restrict: "A",
-      link: function(scope, elem, attrs) {
-        if (!(attrs.limitTo === "" || angular.isUndefined(attrs.limitTo) || attrs.limitTo === null)) {
-          var limit = parseInt(attrs.limitTo);
-          angular.element(elem).on("keypress", function(e) {
-            if (this.value.length == limit) e.preventDefault();
+    //Function to limit the Max Length entered
+    .directive("limitTo", [function () {
+      return {
+        restrict: "A",
+        link: function (scope, elem, attrs) {
+          angular.element(elem).on("keypress", function (e) {
+            if (!(attrs.limitTo === "" || angular.isUndefined(attrs.limitTo) || attrs.limitTo === null)) {
+              var limit = parseInt(attrs.limitTo);
+              if (this.value.length == limit) e.preventDefault();
+            }
           });
         }
-      }
-    };
+      };
   }]);
 
 })();
